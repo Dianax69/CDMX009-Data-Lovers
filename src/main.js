46eb6287363ea28254f9c338d541bf0ed2bb3947
IrisFyD <iriis.trejo.c@gmail.com>
2020-02-28 08:21:47 -0600

    iamagess slider

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-import { example } from './data.js';
+let slider = document.querySelector('.container-slider')
+let sliderIndividual = document.querySelectorAll('.slider-container')
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 5000;
 
-console.log(example);
+window.addEventListener("resize", function() {
+  width = sliderIndividual[0].clientWidth;
+})
+setInterval(function(){
+  slides();
+},intervalo);
 
+function slides(){
+  slider.style.transform = "translate("+(-width*contador)+"px)";
+  slider.style.transition = "transform .8s";
+  contador++;
+
+  if (contador == sliderIndividual.length){
+    setTimeout(function() {
+      slider.style.transform = "translate(0px)";
+      slider.style.transition = "transform 0s";
+      contador=1;
+    },1500)
+  }
+}
