94415646625baca9d6c9e52eefc49ce638fe0415
Dianax69 <58241414+Dianax69@users.noreply.github.com>
2020-03-05 01:09:25 -0600

    added an alias for importjavascript file with data , used a for to show info into javascript to html and an eventlistener to use a button that show another page for choose seasons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // alias for import
-// import data from './data.js';
+import { rickResultData } from './data.js';
 
-
+// function for slider
 let slider = document.querySelector('.container-slider')
 let sliderIndividual = document.querySelectorAll('.slider-container')
 let contador = 1;
@@ -28,8 +28,27 @@ function slides(){
     },1500)
   }
 };
-// function showRandomChar () {
-//   document.getElementById("random-char").style.display = "none";
+
+
+
+const objectRMData = rickResultData();
+for (const index in objectRMData) {
+  const idCharacter = objectRMData[index].id;
+  const nameCharacter = objectRMData[index].name;
+  const htmlCardContainerId = `<li>"${idCharacter}"</li>`;
+  const htmlCardContainerName = `<li>"${nameCharacter}"</li>`;
+  document.getElementById('id-character').append(htmlCardContainerId);
+  document.getElementById('id-character').append(htmlCardContainerName);
+}
+
+document.getElementById('seasons-button').addEventListener('click', () => {
+  document.getElementById('seasons').style.display = 'block';
+});
+
+// window.showSeasons = () => {
+//   // eslint-disable-next-line no-console
+//   console.log('data', Data);
+//   document.getElementById('char-cointainer').style.display = 'block';
 // };
 
-// window.showRandomChar = showRandomChar;
+// window.showSeasons = showSeasons;
