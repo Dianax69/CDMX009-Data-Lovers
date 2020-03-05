ae2369f308bc9d99ae28299863759fd49ad2d087
Dianax69 <58241414+Dianax69@users.noreply.github.com>
2020-03-05 01:03:33 -0600

    import javascript data object to use into a export function

--- src/data.js
+++ src/data.js
@@ -1,43 +1,6 @@
-import data from 'file://../data/rickandmorty/rickandmorty.js';
-
-let charContainer = document.getElementById("char-cointainer");
-const buttonChar = document.getElementById("button-char");
-
-  buttonChar.addEventListener("click", function() {
-    const requestRandomChar = new XMLHttpRequest();
-    requestRandomChar.open("GET", "../data/rickandmorty/rickandmorty.json");
-    requestRandomChar.onload = function() {
-      let ourData = JSON.parse(requestRandomChar.responseText);
-      renderHTML(ourData);
-    };
-    requestRandomChar.send();
-};
-
-function renderHTML(data) {
-  let htmlString = "";
-  for (i = 0; i < data.lenght; i++){
-    htmlString += "<p>" + data[i].name + "is a" + data[i].specie + ".</p>";
-  }
-  charContainer.insertAdjacentHTML("beforeend", htmlString);
-};
-
-function requestRandomChar() {
-  document.getElementById("button-char").value = document.getElementById("button-char").value;
-}
-
-  function showRandomChar () {
-    document.getElementById('random-char').click();
-  }
-  console.log(showRandomChar, "hola")
-  
-  window.showRandomChar = showRandomChar;
-
-  import data from './data/rickandmorty/rickandmorty.js';
-
-let characters = {
+import data from './data/rickandmorty/rickandmorty.js';
 
 
-}
-export const example = () => {
-  return 'example';
-};
+export const rickInfoData = () => data.info;
+
+export const rickResultData = () => data.results;
