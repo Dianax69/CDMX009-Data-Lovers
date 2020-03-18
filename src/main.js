be16092db2f0095d193023b126dbf2242c3cee7a
Dianax69 <58241414+Dianax69@users.noreply.github.com>
2020-03-17 20:13:00 -0600

    fixed eslint errors in slider

--- src/main.js
+++ src/main.js
@@ -4,32 +4,32 @@ import { rickResultData } from './data.js';
 // declare const with a array values
 const objectRMData = rickResultData();
 // function for slider
-let slider = document.querySelector('.container-slider')
-let sliderIndividual = document.querySelectorAll('.slider-container')
+const slider = document.querySelector('.container-slider');
+const sliderIndividual = document.querySelectorAll('.slider-container');
 let contador = 1;
 let width = sliderIndividual[0].clientWidth;
-let intervalo = 2500;
+const intervalo = 2500;
 
-window.addEventListener("resize", function() {
-  width = sliderIndividual[0].clientWidth;
-})
-setInterval(function(){
-  slides();
-},intervalo);
-
-function slides(){
-  slider.style.transform = "translate("+(-width*contador)+"px)";
-  slider.style.transition = "transform .8s";
+function slides() {
+  slider.style.transform = `translate(${-width * contador}px)`;
+  slider.style.transition = 'transform .8s';
   contador++;
 
-  if (contador == sliderIndividual.length){
-    setTimeout(function() {
-      slider.style.transform = "translate(0px)";
-      slider.style.transition = "transform 0s";
-      contador=1;
-    },1500)
+  if (contador === sliderIndividual.length) {
+    setTimeout(() => {
+      slider.style.transform = 'translate(0px)';
+      slider.style.transition = 'transform 0s';
+      contador = 1;
+    }, 1500);
   }
-};
+}
+
+window.addEventListener('resize', () => {
+  width = sliderIndividual[0].clientWidth;
+});
+setInterval(() => {
+  slides();
+}, intervalo);
 
 const randomChars = (obj) => {
   const newRandomArray = [];
